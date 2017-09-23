04620696b5b0859ad9f4a1f0ff9a87de6bb98e2a
yiru99 <yiru82@gmail.com>
2017-09-23 15:43:40 -0700

    init

---
+++ Place_People/Place>People/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Place>People
+//
+//  Created by Yiru Yao on 9/23/17.
+//  Copyright © 2017 APIWorldHackathon. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
---
+++ Place_People/Place>People/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Place>People
+//
+//  Created by Yiru Yao on 9/23/17.
+//  Copyright © 2017 APIWorldHackathon. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+
+@interface ViewController : UIViewController
+
+
+@end
