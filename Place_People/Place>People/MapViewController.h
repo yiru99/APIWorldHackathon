0ced1526b033b47c2fce561cf534cd2963bd353b
kelvin <there.are.no.mails@gmail.com>
2017-09-23 23:45:19 -0700

    Added MapViewController

---
+++ Place_People/Place>People/MapViewController.h
@@ -0,0 +1,13 @@
+//
+//  MapViewController.h
+//  Place>People
+//
+//  Created by kelvin on 23/9/17.
+//  Copyright © 2017 APIWorldHackathon. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface MapViewController : NSObject
+
+@end
---
+++ Place_People/Place>People/MapViewController.m
@@ -0,0 +1,13 @@
+//
+//  MapViewController.m
+//  Place>People
+//
+//  Created by kelvin on 23/9/17.
+//  Copyright © 2017 APIWorldHackathon. All rights reserved.
+//
+
+#import "MapViewController.h"
+
+@implementation MapViewController
+
+@end
